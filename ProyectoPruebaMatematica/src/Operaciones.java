3a8ea46db94ad93bf5e83ef7b2d9acaaed373e67
itorres2607 <ismatorr@gmail.com>
2014-10-22 09:06:55 -0500

    Upload the main project

---
+++ ProyectoPruebaMatematica/src/Operaciones.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Isaac
+ */
+public class Operaciones {
+    
+    private int x;
+    private int y;
+   
+    public Operaciones(int x , int y)
+    {
+        this.x = x;
+        this.y = y;
+        
+    }
+    
+    public int suma()
+    {
+        return x+y;
+    }
+}
